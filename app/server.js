a0435b5c49cd9cc2a23acfe3fb157ec2b7427f4a
Jeff Brimager <sam1el@github.com>
2025-04-13 16:57:48 -0500

    working code
    Signed-off-by: Jeff Brimager <sam1el@github.com>

---
+++ app/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+// Retrieve MOTD from environment variables with a fallback
+const motd = process.env.MOTD || 'Default Message';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send(`Hello World ${motd}\n`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
